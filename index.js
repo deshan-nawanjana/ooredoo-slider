43d31592344df6821cd507dce822a02b05f9c09b
Deshan Nawanjana <deshan.uok@gmail.com>
2023-06-10 23:56:25 +0530

    Position Fixed Mechanism

--- index.js
+++ index.js
@@ -1,153 +1,117 @@
-// get conatiner
-const conatiner = document.querySelector('.slide-container')
+// slider wrapper element
+const wrapper = document.querySelector('.slider-wrapper')
+// slider container element
+const container = document.querySelector('.slider-container')
+// navbar element
+const navbar = document.querySelector('.nav-wrap')
 
-// method to get slide
-const getSlide = () => {
-    // get class name
-    const name = conatiner.className
-    // return calculated slide index
-    return parseInt(name[name.lastIndexOf('slide-') + 6])
-}
+// slide steps array
+const steps = [
+    { parent: 1, child: 1 },
+    { parent: 2, child: 1 },
+    { parent: 3, child: 1 },
+    { parent: 3, child: 2 },
+    { parent: 3, child: 3 },
+    { parent: 4, child: 1 },
+    { parent: 4, child: 2 },
+    { parent: 4, child: 3 },
+    { parent: 5, child: 1 }
+]
 
-// method to get step
-const getStep = () => {
-    // get class name
-    const name = conatiner.className
-    // return calculated step index
-    return parseInt(name[name.lastIndexOf('step-') + 5])
-}
+// current step
+let current = steps[0]
 
-// method to set slide
-const setSlide = (index, step = 1) => {
-    // set container class
-    conatiner.className = `slide-container slide-${index} step-${step}`
-}
-
-// method to set step
-const setStep = index => {
-    // set container class
-    conatiner.className = `slide-container slide-${getSlide()} step-${index}`
+// method to set slider steps
+const sliderStep = (steps, override = false) => {
+    // return non override sliding with manual child steps
+    if (current.parent === 5 && steps.parent === 5 && override === false) { return }
+    if (current.parent === 1 && steps.parent === 1 && override === false) { return }
+    // remember current steps
+    current = steps
+    // set step attribute
+    container.setAttribute('step', steps.parent + ':' + steps.child)
 }
 
 // click event listener
-conatiner.addEventListener('click', event => {
+window.addEventListener('click', event => {
     // get target element
     const target = event.target
-    // get step index
-    let step = getStep()
+    // clone current step
+    const step = Object.assign({}, current)
     // check for swipe text
-    if (target.classList.contains('text-swipe-button')) {
-        // get next step
-        step += target.classList.contains('left') ? -1 : 1
+    if (target.classList.contains('text-swap-button')) {
+        // update next step
+        step.child += target.classList.contains("left") ? -1 : 1
         // reset step if need
-        if (step > 3) { step = 1 }
+        if (step.child > 2) { step.child = 1 }
         // reverse step if need
-        if (step < 1) { step = 3 }
-        // set next step
-        setStep(step)
+        if (step.child < 1) { step.child = 2 }
+        // set slider step
+        sliderStep(step, true)
     }
-    // check for snap card
-    if (target.classList.contains('snap-item')) {
-        // get step on attribute
-        step = parseInt(target.getAttribute('step'))
-        // set next step
-        setStep(step)
+    // check for screen card
+    if (target.classList.contains('screen-item')) {
+        // set child from attribute
+        step.child = parseInt(target.getAttribute('child'))
+        // set slider step
+        sliderStep(step, true)
     }
 })
 
-// slides and steps array
-const steps = [
-    { slide: 1, step: 1 },
-    { slide: 2, step: 1 },
-    { slide: 3, step: 1 },
-    { slide: 3, step: 2 },
-    { slide: 3, step: 3 },
-    { slide: 4, step: 1 },
-    { slide: 4, step: 2 },
-    { slide: 4, step: 3 },
-    { slide: 5, step: 1 }
-]
-
-// main ribbon height
-let ribbon = 100
-// wheel height
-let wheel = 600
-// zero delta flag
-let zdelta = false
-// mobile mode flag
-let mobile = false
-// slider active state
-let active = false
-// last step by update
-let last = null
-
-const updateSlidePosition = loop => {
-    // return in mobile mode
-    if (mobile) { return }
-    // get container bound
-    const bound = conatiner.getBoundingClientRect()
-    // calculate values
-    const position = bound.top - ribbon
-    const absolute = Math.abs(position)
-    const duration = wheel * steps.length
-    // check bound position
-    if (position < 0 && absolute < duration) {
-        // relocate container position in active
-        conatiner.style.paddingTop = absolute + 'px'
-        // calculate step index from position
-        const index = parseInt(steps.length * absolute / duration)
-        // set slide if available
-        if (index in steps && last !== index) {
-            last = index
-            // get item from steps
-            const item = steps[index]
-            // set slide and step
-            setSlide(item.slide, item.step)
-        }
-        // update active state
-        active = true
-    } else if (absolute > duration) {
-        // complete container position in lower space
-        conatiner.style.paddingTop = duration + 'px'
-        // update active state
-        active = false
+// method to update slider
+const updateSlider = () => {
+    // get navbar height
+    const navbar_height = navbar.getBoundingClientRect().height
+    // set navbar height property
+    wrapper.style.setProperty('--navbar-height', navbar_height.toFixed(4) + 'px')
+    // get wrapper bounding rect
+    const wrapper_rect = wrapper.getBoundingClientRect()
+    // get header position of wrapper
+    const head_position = wrapper_rect.top
+    // get footer position of wrapper
+    const foot_position = wrapper_rect.bottom
+    // get wrapper height
+    const wrapper_height = wrapper_rect.height
+    // check for header overtake
+    if (head_position < navbar_height) {
+        // set active state
+        container.setAttribute('active', '')
     } else {
-        // reset container position in upper space
-        conatiner.style.paddingTop = '0px'
-        // update active state
-        active = false
+        // remove active state
+        container.removeAttribute('active')
+        // set first slider step
+        sliderStep(steps[0], true)
     }
-    // request next frame for loop mood
-    if (loop) { requestAnimationFrame(updateSlidePosition) }
+    // check for footer overtake
+    if (foot_position < window.innerHeight) {
+        // calculate custom top position
+        const top = (window.innerHeight - foot_position - navbar_height) * -1
+        // set container top position
+        container.style.top = top + 'px'
+        // set last slider step
+        sliderStep(steps[steps.length - 1])
+    } else {
+        // remove custom styles
+        container.removeAttribute('style')
+    }
+    // check for inner scroll
+    if (head_position < navbar_height && foot_position > window.innerHeight) {
+        // get current position
+        const crr_position = Math.abs(head_position - navbar_height)
+        // get maximum position
+        const max_position = wrapper_height - window.innerHeight + navbar_height
+        // get step index
+        const step_index = parseInt(steps.length * crr_position / max_position)
+        // set current slider step
+        sliderStep(steps[step_index])
+    }
 }
 
-// update slider on scroll
-window.addEventListener('scroll', () => updateSlidePosition(false))
-// start update slider loop on window load
-window.addEventListener('load', () => updateSlidePosition(true))
+// update silder onscroll
+window.addEventListener('scroll', updateSlider)
 
-// none passive wheel event listener
-window.addEventListener('wheel', event => {
-    // update zero delta flag
-    if (event.deltaY === 0) { zdelta = true }
-    // set wheel height by zero delta
-    wheel = zdelta ? 550 : Math.abs(event.deltaY) * 4
-    // check for mouse wheel input mode
-    if (zdelta === false) {
-        // return if first step and wheel up
-        if (last === 0 && event.deltaY < 0) { return }
-        // return if last step and wheel down
-        if (last === steps.length - 1 && event.deltaY > 0) { return }
-    }
-    // check for active state
-    if (active) {
-        // set manual scroll position
-        document.documentElement.scrollTop += event.deltaY
-        // prevent default
-        event.preventDefault()
-        // stop propagation
-        event.stopPropagation()
-        // return false
-        return false
-    }
-}, { passive: false })
+// update silder onload
+window.addEventListener('load', updateSlider)
+
+// update silder resize
+window.addEventListener('resize', updateSlider)
